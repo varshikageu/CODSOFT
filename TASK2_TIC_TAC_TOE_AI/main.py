032ad53ea48ab2a2520e1d378db5b3210cd14fae
varshikageu <varshikasaini17@gmail.com>
2025-11-04 22:53:44 +0530

    Delete TASK2_TIC_TAC_TOE_AI directory

--- TASK2_TIC_TAC_TOE_AI/main.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from gui.tic_tac_toe_gui import TicTacToeGUI
-if __name__ == "__main__":
-    window = tk.Tk()
-    window.title("Tic-Tac-Toe vs AI")
-    window.geometry("400x400")  
-    game_app = TicTacToeGUI(window)
-
-    window.mainloop()
