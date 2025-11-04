2e9045dfec6b98f1916532af620a5e16a8ca9ea1
varshikageu <varshikasaini17@gmail.com>
2025-11-05 00:44:22 +0530

    Fixed import issues and updated main.py

--- TASK2_TIC_TAC_TOE/main.py
+++ TASK2_TIC_TAC_TOE/main.py
@@ -1,5 +1,11 @@
+import sys
+import os
+
+# Add absolute gui folder to Python path
+sys.path.append(os.path.join(r"C:\Users\Varshika\Desktop\rep\CODSOFT\TASK2_TIC_TAC_TOE", "gui"))
+
 import tkinter as tk
-from gui.tic_tac_toe_gui import TicTacToeGUI
+from tic_tac_toe_gui import TicTacToeGUI  
 if __name__ == "__main__":
     window = tk.Tk()
     window.title("Tic-Tac-Toe vs AI")
